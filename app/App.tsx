c8194b06558abe1c072858a27267f6cc8b420767
Netanel Dadon <netanel83@hotmail.com>
2023-08-04 17:04:31 +0300

    add flowise chat

---
+++ app/App.tsx
@@ -0,0 +1,10 @@
+import { BubbleChat } from 'flowise-embed-react'
+
+const App = () => {
+
+    return (
+        <BubbleChat chatflowid="0e95de8a-00cd-4873-93c7-7bfadb159ae8" apiHost="https://flowise-smgx.onrender.com" />
+    );
+};
+
+export default App
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
-
+module.exports = {
+    experimental: {
+      urlImports: ['https://cdn.jsdelivr.net/npm/flowise-embed/dist/', 'https://flowise-smgx.onrender.com'],
+    },
+  }
 module.exports = nextConfig
