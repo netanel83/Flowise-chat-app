fc0843688e2e3c1741fc95e1ca66c557ea0eadf5
Netanel Dadon <netanel83@hotmail.com>
2023-08-04 17:12:43 +0300

    title

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Flowise Demo</h1>
       <App/>
     </main>
   )
